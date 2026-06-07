dcb558c7855415923de1e138aeb7df15804cc599
mcardle9ian <ian@crossoverresearch.com>
2026-06-07 13:52:50 -0400

    Fix stray bracket in conservice layout

--- app/conservice/layout.tsx
+++ app/conservice/layout.tsx
@@ -6,5 +6,5 @@ export const metadata: Metadata = {
 };
 
 export default function ConserviceLayout({ children }: { children: React.ReactNode }) {
-    return <>{children}</>>;
+    return <>{children}</>;
 }
